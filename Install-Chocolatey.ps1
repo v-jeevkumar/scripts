672787fbac9651650aa1bac0bc3888943a61cb64
satudum <98588179+satudum@users.noreply.github.com>
2022-06-09 22:27:55 +0530

    Create Install-Chocolatey.ps1

---
+++ Install-Chocolatey.ps1
@@ -0,0 +1,29 @@
+function Install-Chocolatey {
+    $chocoExePath = 'C:\ProgramData\Chocolatey\bin'
+
+    if ($($env:Path).ToLower().Contains($($chocoExePath).ToLower())) {
+
+        exit
+    } else {
+        Write-Log "Installing Chocolatey..."
+        # Add to system PATH
+        $systemPath = [environment]::GetEnvironmentVariable('Path',[System.EnvironmentVariableTarget]::Machine)
+        $systemPath += ';' + $chocoExePath
+        [environment]::SetEnvironmentVariable("PATH",$systemPath,[System.EnvironmentVariableTarget]::Machine)
+
+        # Update local process' path
+        $userPath = [environment]::GetEnvironmentVariable('Path',[System.EnvironmentVariableTarget]::User)
+        if ($userPath) {
+            $env:Path = $systemPath + ";" + $userPath
+        } else {
+            $env:Path = $systemPath
+        }
+
+        # Run the installer
+        Invoke-Expression ((New-Object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+    }
+    choco feature enable --name allowGlobalConfirmation # stop the -y flag being needed for all "choco install"s
+    choco feature disable --name checksumFiles # lots of packages have no checksums, e.g. WinSDK, so allow them
+    choco install -y vcredist140
+}
+Install-Chocolatey
